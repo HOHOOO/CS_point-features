ad8bfe49529b7424664d03a6e7cbbffdf8c31a72
HOHO <hohooo@live.com>
2016-11-28 11:33:19 +0800

    Update sparkr_cluster_point.r

--- sparkr_cluster_point.r
+++ sparkr_cluster_point.r
@@ -104,99 +104,14 @@ end_rdd_rdd <- SparkR:::flatMapValues(end_rdd, function(x) {
 })
 end_rdd_value<-SparkR:::values(end_rdd_rdd)
 SparkR:::cache(end_rdd_value)
-SparkR:::saveAsTextFile(end_rdd_value, "/user/kettle/ubi/dm/ubi_dm_cluster_point/stat_date=XXXXXX")
 
-######## WARNING: run in hive #####################################
-CREATE external TABLE ubi_dw_cluster_point_XXXXXX (deviceid String,tid String,vid String,start String,actual_start String,s_end String,dura DOUBLE,period String,lat_st_ori DOUBLE,lon_st_ori DOUBLE,lat_en_ori DOUBLE,lon_en_ori DOUBLE,m_ori DOUBLE,lat_st_def DOUBLE,lon_st_def DOUBLE,lat_en_def DOUBLE,lon_en_def DOUBLE,m_def DOUBLE,speed_mean DOUBLE,gps_speed_sd DOUBLE,gps_acc_sd DOUBLE,dura2 String,sort_st String,sort_en String,stat_date string) ROW format delimited FIELDS TERMINATED BY ',' LOCATION '/user/kettle/ubi/dw/cluster_point/stat_date=XXXXXX';
+SparkR:::saveAsTextFile(end_rdd_value, paste0("/user/kettle/ubi/dw/cluster_point/stat_date=",date_period))
+sql(hiveContext,"drop TABLE ubi_dw_cluster_point")
+sql(hiveContext,"CREATE external TABLE ubi_dw_cluster_point (deviceid String,tid String,vid String,start String,actual_start String,s_end String,dura DOUBLE,period String,lat_st_ori DOUBLE,lon_st_ori DOUBLE,lat_en_ori DOUBLE,lon_en_ori DOUBLE,m_ori DOUBLE,lat_st_def DOUBLE,lon_st_def DOUBLE,lat_en_def DOUBLE,lon_en_def DOUBLE,m_def DOUBLE,speed_mean DOUBLE,gps_speed_sd DOUBLE,gps_acc_sd DOUBLE,dura2 String,sort_st String,sort_en String,dim_month string) partitioned BY (stat_date string) ROW format delimited FIELDS TERMINATED BY ',' LOCATION '/user/kettle/ubi/dw/cluster_point/'")
+sql(hiveContext,paste0("ALTER TABLE ubi_dw_cluster_point add PArtition (stat_date='",date_period,"')"))
+sql(hiveContext,"insert overwrite table ubi_dw_cluster_point select trim(deviceid) as deviceid,trim(tid) as tid,trim(vid) as vid,trim(start) as start,trim(actual_start) as actual_start,trim(s_end) as s_end,dura,trim(period) as period,lat_st_ori,lon_st_ori,lat_en_ori,lon_en_ori,m_ori,lat_st_def,lon_st_def,lat_en_def,lon_en_def,m_def,speed_mean,gps_speed_sd,gps_acc_sd,trim(dura2) as dura2,trim(sort_st) as sort_st,trim(sort_en) as sort_en,trim(stat_date) as stat_date from ubi_dw_cluster_point")
+sql(hiveContext,"drop TABLE ubi_dw_cluster_point")
+sql(hiveContext,"CREATE external TABLE ubi_dw_cluster_point (deviceid String,tid String,vid String,start String,actual_start String,s_end String,dura DOUBLE,period String,lat_st_ori DOUBLE,lon_st_ori DOUBLE,lat_en_ori DOUBLE,lon_en_ori DOUBLE,m_ori DOUBLE,lat_st_def DOUBLE,lon_st_def DOUBLE,lat_en_def DOUBLE,lon_en_def DOUBLE,m_def DOUBLE,speed_mean DOUBLE,gps_speed_sd DOUBLE,gps_acc_sd DOUBLE,dura2 String,sort_st String,sort_en String,dim_month string) partitioned BY (stat_date string) ROW format delimited FIELDS TERMINATED BY ',' LOCATION '/user/kettle/ubi/dw/cluster_point/'")
+sql(hiveContext,paste0("ALTER TABLE ubi_dw_cluster_point add PArtition (stat_date='",date_period,"')"))
 
-
-
-
-######## Not run: ###### debugs spark 1.6 #####################################
-遗留问题：
-1.动态建表hive中按天分区是的time.out问题；
-2.建表中的 问题。（先将int变为了string）
-########1. register dynamic.partitions table #####################################
-end_end_rdd<-SparkR:::toDF(end_rdd_value,list('deciveid','tid','vid','start','actual_start','s_end','dura','period','lat_st_ori','lon_st_ori','lat_en_ori','lon_en_ori','m_ori','lat_st_def','lon_st_def','lat_en_def','lon_en_def','m_def','speed_mean','gps_speed_sd','gps_acc_sd','dura2','sort_st','sort_en','stat_date'))
-registerTempTable(end_end_rdd,"cluster_point")
-sql(hiveContext,"set hive.exec.dynamic.partition.mode=nostrick")
-sql(hiveContext,"set hive.exec.dynamic.partition=true")
-sql(hiveContext,"set hive.exec.max.dynamic.partitions.pernode = 2000000000")
-sql(hiveContext,"set hive.exec.max.dynamic.partitions = 2000000000")
-sql(hiveContext,"set hive.exec.max.created.files = 2000000000")
-sql(hiveContext,"insert overwrite table ubi_dm_cluster_point partition (stat_date) select * from cluster_point")
-
-test：
-randomMatBr <- broadcast(sc, randomMat)
-connectBackend.orig <- getFromNamespace('connectBackend', pos='package:SparkR')
-connectBackend.patched <- function(hostname, port, timeout = 3600*48) {
-   connectBackend.orig(hostname, port, timeout)
-}
-assignInNamespace("connectBackend", value=connectBackend.patched, pos='package:SparkR')
-
-########2. factor problem #####################################
-"start String,actual_start String,s_end String"
-in R may cause factor problem
-RUN:
-col <- as.vector(unlist(col))
-mode(col) <- "numeric"
-ps:解决空格问题，不解决数据类型问题
-sqlstr1+=" insert overwrite table ubi_dw_cluster_point_201601 select "
-sqlstr1+=" trim(deviceid) as deviceid,"
-sqlstr1+=" trim(tid) as tid,"
-sqlstr1+=" trim(vid) as vid,"
-sqlstr1+=" trim(start) as start,"
-sqlstr1+=" trim(actual_start) as actual_start,"
-sqlstr1+=" trim(s_end) as s_end,"
-sqlstr1+=" dura,"
-sqlstr1+=" trim(period) as period,"
-sqlstr1+=" lat_st_ori,"
-sqlstr1+=" lon_st_ori,"
-sqlstr1+=" lat_en_ori,"
-sqlstr1+=" lon_en_ori,"
-sqlstr1+=" m_ori,"
-sqlstr1+=" lat_st_def,"
-sqlstr1+=" lon_st_def,"
-sqlstr1+=" lat_en_def,"
-sqlstr1+=" lon_en_def,"
-sqlstr1+=" m_def,"
-sqlstr1+=" speed_mean,"
-sqlstr1+=" gps_speed_sd,"
-sqlstr1+=" gps_acc_sd,"
-sqlstr1+=" trim(dura2) as dura2,"
-sqlstr1+=" trim(sort_st) as sort_st,"
-sqlstr1+=" trim(sort_en) as sort_en,"
-sqlstr1+=" trim(stat_date) as stat_date"
-sqlstr1+=" from"
-sqlstr1+=" ubi_dw_cluster_point_201601"
-
-
-insert overwrite table ubi_dw_cluster_point_201607 select 
-trim(deviceid) as deviceid,
-trim(tid) as tid,
-trim(vid) as vid,
-trim(start) as start,
-trim(actual_start) as actual_start,
-trim(s_end) as s_end,
-dura,
-trim(period) as period,
-lat_st_ori,
-lon_st_ori,
-lat_en_ori,
-lon_en_ori,
-m_ori,
-lat_st_def,
-lon_st_def,
-lat_en_def,
-lon_en_def,
-m_def,
-speed_mean,
-gps_speed_sd,
-gps_acc_sd,
-trim(dura2) as dura2,
-trim(sort_st) as sort_st,
-trim(sort_en) as sort_en,
-trim(stat_date) as stat_date
-from
-ubi_dw_cluster_point_201607
 
