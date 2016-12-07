d8203fbdc0df5d8bc0268dddfe8abf749d872984
HOHO <hohooo@live.com>
2016-12-07 17:29:41 +0800

    Add files via upload

---
+++ spark-submit_ubi_dw_cluster_point.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ $# -lt 1 ];
+    then
+    #stat_date=''
+    echo "please input month paras eg:201607"
+	exit 1
+else
+    stat_date=$1
+fi
+
+for arg in "$@"
+do
+    echo last month args: $arg
+done
+stat_date=$1
+/opt/cloudera/parcels/SPARK/bin/spark-submit --driver-memory 20g --master spark://hadoop-namenode1:7077 /opt/data-platform/ml/r/ubi_dw_cluster_point.R $stat_date
